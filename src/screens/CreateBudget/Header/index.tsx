3c08e994f8ca09ccf26a78194488b66cf3949810
Marcelo Galdino <marcelinhomgo@gmail.com>
2026-01-20 23:58:17 -0300

    feat: CreateBudget screens components

--- src/screens/CreateBudget/Header/index.tsx
+++ src/screens/CreateBudget/Header/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import { ChevronLeft } from "@/assets/icons/ChevronLeft";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export const Header = () => {
   return (
-    <View className="bg-white w-full h-16 border border-gray-200">
-      <Text></Text>
+    <View className="bg-white flex-row items-center justify-start gap-4 p-5 w-full h-16 border-b border-gray-200">
+      <TouchableOpacity>
+        <ChevronLeft />
+      </TouchableOpacity>
+      <Text className="font-bold text-sm leading-5">Orçamento</Text>
     </View>
   );
 };
--- src/screens/CreateBudget/index.tsx
+++ src/screens/CreateBudget/index.tsx
@@ -1,5 +1,147 @@
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import { Header } from "./Header";
+import { Button } from "@/components/Button";
+import { Check } from "@/assets/icons/Check";
+import { Shop } from "@/assets/icons/Shop";
+import { Tag } from "@/assets/icons/Tag";
+import { Status } from "@/components/Status";
+import { StatusTypes } from "@/components/Status/strategies/status-stategy";
+import { NoteWithText } from "@/assets/icons/NoteWithText";
+import { Plus } from "@/assets/icons/Plus";
+import { CreditCard } from "@/assets/icons/CreditCard";
 
 export const CreateBudget = () => {
-  return <Header />;
+  return (
+    <View className="flex-1 bg-white">
+      <Header />
+
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={0}
+      >
+        <ScrollView
+          style={{ flex: 1 }}
+          contentContainerStyle={{ padding: 20, paddingBottom: 30 }}
+          keyboardShouldPersistTaps="handled"
+          showsVerticalScrollIndicator={false}
+        >
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View>
+              <View className="border border-gray-300 rounded-lg mb-5">
+                <View className="h-[40px] border-b border-gray-300 p-3">
+                  <View className="flex-row items-center justify-start gap-2">
+                    <Shop size={14} color="#6A46EB" />
+                    <Text className="font-normal text-xs text-gray-500">
+                      Informações gerais
+                    </Text>
+                  </View>
+                </View>
+
+                <View className="p-4 gap-2">
+                  <TextInput
+                    className="w-full h-[48px] bg-gray-100 border border-gray-300 rounded-full px-4 py-3 text-base font-normal"
+                    placeholder="Título"
+                  />
+
+                  <TextInput
+                    className="w-full h-[48px] bg-gray-100 border border-gray-300 rounded-full px-4 py-3 text-base font-normal"
+                    placeholder="Cliente"
+                  />
+                </View>
+              </View>
+
+              <View className="border border-gray-300 rounded-lg mb-5">
+                <View className="h-[40px] border-b border-gray-300 p-3">
+                  <View className="flex-row items-center justify-start gap-2">
+                    <Tag size={14} color="#6A46EB" />
+                    <Text className="font-normal text-xs text-gray-500">
+                      Status
+                    </Text>
+                  </View>
+                </View>
+
+                <View className="p-4 gap-2">
+                  <View className="flex-row justify-around">
+                    <Status statusType={StatusTypes.RASCUNHO} />
+                    <Status statusType={StatusTypes.APROVADO} />
+                  </View>
+                  <View className="flex-row justify-around">
+                    <Status statusType={StatusTypes.ENVIADO} />
+                    <Status statusType={StatusTypes.RECUSADO} />
+                  </View>
+                </View>
+              </View>
+
+              <View className="border border-gray-300 rounded-lg mb-5">
+                <View className="h-[40px] border-b border-gray-300 p-3">
+                  <View className="flex-row items-center justify-start gap-2">
+                    <NoteWithText size={14} color="#6A46EB" />
+                    <Text className="font-normal text-xs text-gray-500">
+                      Serviços inclusos
+                    </Text>
+                  </View>
+                </View>
+
+                <View className="p-4 gap-2">
+                  <TextInput
+                    className="w-full h-[48px] bg-gray-100 border border-gray-300 rounded-full px-4 py-3 text-base font-normal"
+                    placeholder="Título"
+                  />
+
+                  <TextInput
+                    className="w-full h-[48px] bg-gray-100 border border-gray-300 rounded-full px-4 py-3 text-base font-normal"
+                    placeholder="Cliente"
+                  />
+
+                  <Button
+                    name="Adiocionar serviço"
+                    mode="outline"
+                    icon={<Plus color="#6A46EB" />}
+                  />
+                </View>
+              </View>
+
+              <View className="border border-gray-300 rounded-lg mb-5">
+                <View className="h-[40px] border-b border-gray-300 p-3">
+                  <View className="flex-row items-center justify-start gap-2">
+                    <CreditCard size={14} color="#6A46EB" />
+                    <Text className="font-normal text-xs text-gray-500">
+                      Investimento
+                    </Text>
+                  </View>
+                </View>
+
+                <View className="p-4 gap-2">
+                  <TextInput
+                    className="w-full h-[48px] bg-gray-100 border border-gray-300 rounded-full px-4 py-3 text-base font-normal"
+                    placeholder="Título"
+                  />
+
+                  <TextInput
+                    className="w-full h-[48px] bg-gray-100 border border-gray-300 rounded-full px-4 py-3 text-base font-normal"
+                    placeholder="Cliente"
+                  />
+                </View>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </ScrollView>
+
+        <View className="flex-row justify-center items-center gap-4 px-5 pt-5 pb-10 w-full bg-white border-t border-gray-200">
+          <Button name="Cancelar" mode="outline" />
+          <Button name="Salvar" icon={<Check color="#FFFFFF" />} />
+        </View>
+      </KeyboardAvoidingView>
+    </View>
+  );
 };
