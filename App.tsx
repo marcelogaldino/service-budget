1aa07b8507d664fff9482e1d16e2a7aee3717966
Marcelo Galdino <marcelinhomgo@gmail.com>
2026-01-27 07:28:12 -0300

    config: icons, splashscreens and development build

--- App.tsx
+++ App.tsx
@@ -6,9 +6,26 @@ import { StorageFactory } from "@/shared/storage/StorageFactory";
 import { STORAGE_CONFIG } from "@/config/storage.config";
 import { BottomSheetProvider } from "@/context/bottomsheet.context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [isReady, setIsready] = useState(false);
   StorageFactory.createStorage(STORAGE_CONFIG.type);
+
+  SplashScreen.preventAutoHideAsync();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsready(true);
+    }, 2000);
+  }, []);
+
+  useEffect(() => {
+    if (isReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [isReady]);
 
   return (
     <GestureHandlerRootView>
